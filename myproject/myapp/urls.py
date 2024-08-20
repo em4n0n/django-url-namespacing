8856b7a6631b8db39530cb0624ae84605bf3f222
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-08-19 17:32:17 -0700

    Add drinks path

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-
+app_name='myapp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
+    path('drinks/<str:drink_name>', views.drinks, name="drink_name"),
 ]
